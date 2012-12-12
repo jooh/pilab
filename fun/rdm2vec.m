72c849c9c709ac982d2360497a1ee6984f0b2331
Johan Carlin <johan.carlin@gmail.com>
2012-12-12 18:02:23 +0000

    added RSA toolbox-style struct arr compatibility to rdm2vec

--- fun/rdm2vec.m
+++ fun/rdm2vec.m
@@ -1,9 +1,15 @@
-% Convert a 3D stack of RDMs to vector form (lower triangular) with one
-% row per RDM. This extends the Matlab built-in pdist by supporting
-% vectorising multiple distance matrices (stacked in 3rd dim) in one
-% operation.
+% Convert a 3D stack of RDMs (or a struct array with an RDM field) to
+% vector form (lower triangular) with one row per RDM. This extends the
+% Matlab built-in pdist by supporting vectorising multiple distance
+% matrices (stacked in 3rd dim) in one operation.
 % vecs = rdm2vec(rdms)
 function vecs = rdm2vec(rdms)
+
+% unpack RSA toolbox-style rdm struct arrays
+if isstruct(rdms)
+    n = size(rdms(1).RDM,1);
+    rdms = reshape([rdms.RDM],[n n length(rdms)]);
+end
 
 [r,c,z] = size(rdms);
 
