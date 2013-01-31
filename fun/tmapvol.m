790dd3d928a015099b28e29d1481b94428788d5d
Johan Carlin <johan.carlin@gmail.com>
2013-01-31 15:19:56 +0000

    support for refactored Volume class

--- fun/tmapvol.m
+++ fun/tmapvol.m
@@ -5,19 +5,19 @@
 function tvol = tmapvol(designvol,epivol,inds)
 
 if nargin<3
-    inds = 1:designvol.nlabels;
+    inds = 1:designvol.desc.features.nunique.labels;
 end
 nt = length(inds);
 
 % fit
-betas = designvol.data' \ epivol.data;
+betas = designvol.data \ epivol.data;
 
 % get variance estimate for t contrast
-rss = sum((epivol.data - designvol.data'*betas).^2);
-df = epivol.ndata - rank(designvol.data');
+rss = sum((epivol.data - designvol.data*betas).^2);
+df = epivol.nsamples - rank(designvol.data);
 mrss = rss / df;
 % model covariance matrix
-covmat = inv(designvol.data * designvol.data');
+covmat = inv(designvol.data' * designvol.data);
 
 % prepare output matrix
 tdata = NaN([nt epivol.nfeatures]);
@@ -25,14 +25,15 @@ tdata = NaN([nt epivol.nfeatures]);
 % suggests you get better performance without parfor here, so disabled for
 % now)
 colinds = 1:epivol.nfeatures;
-labelinds = designvol.labelinds;
+labelinds = designvol.desc.features.inds.labels;
 % iterate to make t maps
 for t = 1:nt
     % make the vector for this label
-    cv = (labelinds==inds(t))';
+    cv = double(labelinds==inds(t));
     % mean over standard error
     tdata(t,colinds) = cv * betas ./ sqrt(mrss * (cv * covmat * cv'));
 end
 
 % make the Volume instance
-tvol = Volume(tdata,epivol,'labels',designvol.uniquelabels(inds));
+tvol = MriVolume(tdata,epivol,'metasamples',struct('labels',...
+    {designvol.desc.features.unique.labels(inds)'}));
