865f055ea390592f420860b06e798563e0a9859e
Johan Carlin <johan.carlin@gmail.com>
2013-02-15 11:53:21 +0000

    now using CovGLM for fitting with covariates, support for adaptive setting of polynomial degree

--- glm/tmapvol.m
+++ glm/tmapvol.m
@@ -2,15 +2,21 @@
 % tvol = tmapvol(designvol,epivol,[inds])
 %
 % inds are indices into uniquelabels (defaults to 1:nlabels)
-function tvol = tmapvol(designvol,epivol,inds)
+function tvol = tmapvol(designvol,epivol,inds,covariatedeg)
 
 if nargin<3
     inds = 1:designvol.desc.features.nunique.labels;
 end
 nt = length(inds);
 
+if strcmp(covariatedeg,'adaptive')
+    % Kay's rule for deciding on n covariates based on run duration.
+    covariatedeg = round(epivol.nsamples*epivol.frameperiod/60/2);
+    assert(~isempty(covariatedeg),'failed to identify covariatedeg');
+end
+
 % fit
-model = GLM(designvol.data,epivol.data);
+fit = CovGLM(designvol.data,epivol.data,covariatedeg);
 % prepare output matrix
 tdata = NaN([nt epivol.nfeatures]);
 % concessions to parfor to avoid memory overhead (actually, further testing
@@ -18,12 +24,14 @@ tdata = NaN([nt epivol.nfeatures]);
 % now)
 colinds = 1:epivol.nfeatures;
 labelinds = designvol.desc.features.inds.labels;
+regs = zeros(1,fit.npredictors);
 % iterate to make t maps
 for t = 1:nt
-    % make the vector for this label
-    cv = double(labelinds==inds(t));
+    % make the vector for this label (considering any extra nuisance vars)
+    cv = regs;
+    cv(labelinds==inds(t)) = 1;
     % mean over standard error
-    tdata(t,colinds) = model.tmap(cv);
+    tdata(t,colinds) = fit.tmap(cv);
 end
 
 % make the Volume instance
