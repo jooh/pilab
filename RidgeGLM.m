d36a94d4e6bc4ebd6b660f68289e5f77af7f4629
Johan Carlin <johan.carlin@gmail.com>
2013-06-12 17:40:15 +0100

    support for sub-classing

--- RidgeGLM.m
+++ RidgeGLM.m
@@ -1,18 +1,27 @@
 % CovGLM sub-class for regularised ridge fits.
-% gl = RidgeGLM(X,data,k,[covariatedegree]);
+% gl = RidgeGLM(X,data,k,[unscale],[covariatedegree]);
 classdef RidgeGLM < CovGLM
     properties
         k % regularisation parameter (gets scaled by k*nsamples in ridgevec)
+        unscale % return data to original scale (default 0)
     end
 
     methods
-        function gl = RidgeGLM(X,data,k,covariatedegree)
+        function gl = RidgeGLM(X,data,k,unscale,covariatedegree)
+            if nargin == 0
+                % support no input case
+                [X,data,k] = deal([]);
+            end
             % initialise with super-class constructor
             if ieNotDefined('covariatedegree')
                 covariatedegree = [];
             end
             gl = gl@CovGLM(X,data,covariatedegree);
             gl.k = k;
+            if ieNotDefined('unscale')
+                unscale = 0;
+            end
+            gl.unscale = unscale;
         end
 
         function estimates = fit(self)
@@ -21,9 +30,11 @@ classdef RidgeGLM < CovGLM
         % parameter. k can be a scalar
             % check if we can just do one quick fit
             ks = unique(self(1).k);
+            assert(all([self.unscale] == self(1).unscale),...
+                'only 1 unique unscale option is permitted');
             if length(ks)==1
                 estimates = ridgevec(vertcat(self.data),vertcat(self.X),...
-                    self(1).k);
+                    self(1).k,self(1).unscale);
                 return
             end
             % failing that, fit each unique k separately
@@ -32,7 +43,8 @@ classdef RidgeGLM < CovGLM
             X = vertcat(self.X);
             for k = ks
                 inds = self(1).k == k;
-                estimates(:,inds) = ridgevec(data(:,inds),X,k);
+                estimates(:,inds) = ridgevec(data(:,inds),X,k,...
+                    self(1).unscale);
             end
         end
     end
