b9de567b395077c0fa3d6e2a6606d4ff305e4434
Johan Carlin <johan.carlin@gmail.com>
2013-08-16 17:25:26 +0100

    code for EDI style RSA. Likely not something I will be doing but committed here for posterity.

--- MeanDiffRSA.m
+++ MeanDiffRSA.m
@@ -1,18 +1,12 @@
 % RSA sub-class for fitting a single, binary model RDM to data. By
 % de-meaning the data and model the fitted response becomes equivalent to
-% the mean difference between modelrdm==0 and modelrdm==1. This test
-% outperforms conventional rank-based RSA fits for binary predictions.
+% the mean difference between modelrdm==0 and modelrdm==1. Note that
+% statistically this test is nearly equivalent to RankRSA...
 %
 % gl = MeanDiffRSA(modelrdms,datardms)
 classdef MeanDiffRSA < RSA
     methods
         function gl = MeanDiffRSA(modelrdms,datardms)
-            % support special initialisation mode when initialising by
-            % self. This enables bootstrap / permutation resampling
-            if nargin == 1 && isa(modelrdms,class(gl))
-                [modelrdms,datardms] = deal(modelrdms.Xrdm,...
-                    modelrdms.datardm);
-            end
             gl = gl@RSA(modelrdms,datardms);
             % check that X is of the correct type for this class
             assert(gl.npredictors == 1,...
@@ -26,7 +20,7 @@ classdef MeanDiffRSA < RSA
             gl.X(gl.X==uv(2)) = 1;
             gl.X = gl.X - mean(gl.X);
             % de-mean each feature to avoid needing a constant
-            gl.data = gl.data - repmat(mean(gl.data,1),[gl.nsamples, 1]);
+            gl.data = bsxfun(@minus,gl.data,mean(gl.data,1));
         end
     end
 end
