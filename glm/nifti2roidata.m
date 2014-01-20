7749f90159641f25a4a763a87f38195a865c2b75
Johan Carlin <johan.carlin@gmail.com>
2014-01-20 17:18:08 +0000

    convenience function for loading niftis into roidata format

---
+++ glm/nifti2roidata.m
@@ -0,0 +1,30 @@
+% convert a set of nifti volumes to the 'roidata' structure used elsewhere.
+%
+% INPUTS:
+% niftis: char array of niftis (one per contrast)
+% mask: logical 3D of (group) in-mask voxels
+% targetfield: what to name the roidata field that contains the actual data
+% (default 't')
+%
+% roidata = nifti2roidata(niftis,mask,targetfield)
+function roidata = nifti2roidata(niftis,mask,targetfield)
+
+if ieNotDefined('targetfield')
+    targetfield = 't';
+end
+
+V = spm_vol(niftis);
+ncon = numel(V);
+
+% number of valid entries
+nroi = sum(mask(:));
+
+roidata.(targetfield) = NaN([ncon nroi]);
+roidata.cols_roi = cell(1,nroi);
+roidata.rows_contrast = cell(ncon,1);
+
+for n = 1:ncon
+    [~,roidata.rows_contrast{n},~] = fileparts(V(n).fname);
+    xyz = spm_read_vols(V(n));
+    roidata.(targetfield)(n,:) = xyz(mask);
+end
