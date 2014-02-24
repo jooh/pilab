3d3597fd6580383c96bcc6c723ea32cb11d7631f
Johan Carlin <johan.carlin@gmail.com>
2014-02-24 10:50:50 +0000

    contrast vector of same type as n

--- util/allpairwisecontrasts.m
+++ util/allpairwisecontrasts.m
@@ -10,7 +10,7 @@ mask = tril(ones(n,n),-1);
 % convert to linear indices into out matrix
 pos = sub2ind(outsize,r,(1:outsize(2))');
 neg = sub2ind(outsize,c,(1:outsize(2))');
-c = zeros(outsize);
+c = zeros(outsize,class(n));
 c(pos) = 1;
 c(neg) = -1;
 c = c';
