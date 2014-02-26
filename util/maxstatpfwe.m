9f32118a4cf2c5040e353d1cb547e2de575f445b
Johan Carlin <johan.carlin@gmail.com>
2014-02-26 11:54:36 +0000

    refactored to correct p=0 bug and improve speed

--- util/maxstatpfwe.m
+++ util/maxstatpfwe.m
@@ -1,8 +1,13 @@
 % Return the nichols / holmes p value for a null distribution
+% permutations are in rows, features in columns. The first row is assumed
+% to contain the true, unpermuted effect.
+%
 % [pfwe,pthresh] = maxstatpfwe(nulldist)
 function [pfwe,pthresh] = maxstatpfwe(nulldist)
 
 [nperms,ndata] = size(nulldist);
+assert(ndims(nulldist)==2,'can only support 2d inputs, got %dd',...
+    ndims(nulldist));
 
 % assume truestat is first entry in nulldist
 truestat = nulldist(1,:);
@@ -13,7 +18,6 @@ maxstats = max(nulldist,[],2);
 % the FWE p value is the percentile of truestat in the max distribution
 % (note btw that the uncorrected p value never enters into this
 % calculation)
-pfwe = sum(repmat(maxstats,[1 ndata]) > repmat(truestat,[nperms 1]),1) ...
-    / nperms;
+pfwe = sum(bsxfun(@ge,maxstats,truestat),1) / nperms;
 
 pthresh = prctile(maxstats,95);
