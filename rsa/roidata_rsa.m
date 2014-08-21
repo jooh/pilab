3669643c54ffdc89cb1893515dee72596eb71b6f
Johan Carlin <johan.carlin@gmail.com>
2014-08-21 15:10:47 +0100

    more flexible argument handling to support crossvalidated r2 and other custom fit method

--- rsa/roidata_rsa.m
+++ rsa/roidata_rsa.m
@@ -1,22 +1,33 @@
 % Perform rsa mapping on each of the RDMs in the disvol compared to each
 % model RDMs in the predictors struct.
 %
-% Named inputs (default and description):
+% INPUTS
+% disvol        BaseVolume instance containing vectorised dissimilarities
+%                   (or cell array for multiple run support)
+% predictors    model RDMs in struct array form
 %
-% rsaclass: RankRSA RSA subclass to use (default RankRSA)
-% rsaclassargs: {} any varargins for rsaclass
-% nperm: 1 number of permutations to run (1 being the first, true order)
-% nboot: 0 number of bootstrap resamples of the true model
-% splitrsaclass: Custom RSA subclass to use for split data RDMs (default
-% SplitRankRSA)
-% splitrsaclassargs: {} any varargins for splitrsaclass
+% NAMED INPUTS
+%
+% ARGUMENT          DEFAULT DESCRIPTION
+% rsaclass          RankRSA RSA subclass to use (default RankRSA)
+% rsaclassargs      {}      any varargins for rsaclass
+% nperm             1       number of permutations to run (1 being the
+%                               first, true order)
+% nboot             0       number of bootstrap resamples of the true model
+% splitrsaclass     ''      RSA subclass to use for split data RDMs
+% splitrsaclassargs {}      any varargins for splitrsaclass
+% fitmethod         'fit'   RSA class method to call to obtain result
+% fitmethodargs     {}      any additional arguments for fitmethod
+% customfun         []      catch-all custom processing. Gets called as
+%                               res.custom{c} = feval(customfun,model);
 %
 % [res,nulldist,bootdist] = roidata_rsa(disvol,predictors,[varargin])
 function [res,nulldist,bootdist] = roidata_rsa(disvol,predictors,varargin)
 
 getArgs(varargin,{'rsaclass','RankRSA','nperm',1,'nboot',0,...
     'rsaclassargs',{},'splitrsaclass','',...
-    'splitrsaclassargs',{}});
+    'splitrsaclassargs',{},'fitmethod','fit','fitmethodargs',{},...
+    'customfun',[]});
 
 if ~iscell(rsaclassargs)
     % this extra bit of flexibility helps with cases where you just want to
@@ -28,28 +39,32 @@ if ~iscell(splitrsaclassargs)
     splitrsaclassargs = {splitrsaclassargs};
 end
 
+if ~iscell(disvol)
+    disvol = {disvol};
+end
+
 % we now assume that the inputs are super clean (so that we can assume that
 % the data result will be in register with rois)
-assert(~any(isnan(disvol.data(:))),'nans in disvol');
+assert(~any(isnan(disvol{1}.data(:))),'nans in disvol');
 
 npredictor = numel(predictors);
-basedat = NaN([npredictor disvol.nfeatures]);
-cols_roi = {asrow(disvol.meta.features.names)};
+basedat = NaN([npredictor disvol{1}.nfeatures]);
+cols_roi = {asrow(disvol{1}.meta.features.names)};
 rows_contrast = {ascol({predictors.name})};
 
 res = struct('cols_roi',cols_roi,'rows_contrast',rows_contrast,...
     'r',basedat,'pperm',basedat,'medianboot',basedat,...
     'medianste',basedat);
 
-if isfield(disvol.meta.features,'nfeatures')
-    res.nfeatures = disvol.meta.features.nfeatures;
+if isfield(disvol{1}.meta.features,'nfeatures')
+    res.nfeatures = disvol{1}.meta.features.nfeatures;
 end
 
 % segregate the nulldist/bootdist because these tend to blow up in size
 nulldist = struct('cols_roi',cols_roi,'rows_contrast',rows_contrast,...
-    'r',NaN([npredictor disvol.nfeatures nperm]));
+    'r',NaN([npredictor disvol{1}.nfeatures nperm]));
 bootdist = struct('cols_roi',cols_roi,'rows_contrast',rows_contrast,...
-    'r',NaN([npredictor disvol.nfeatures nboot]));
+    'r',NaN([npredictor disvol{1}.nfeatures nboot]));
 
 for c = 1:npredictor
     if issplitdatardm(predictors(c).RDM)
@@ -62,16 +77,26 @@ for c = 1:npredictor
         args = rsaclassargs;
     end
     % setup the instance
-    model = feval(constructor,predictors(c).RDM,disvol.data,args{:});
+    model = cellfun(@(thisdis)feval(constructor,predictors(c).RDM,...
+        thisdis.data,args{:}),disvol,'uniformoutput',0);
+    if iscell(model)
+        model = cat(1,model{:});
+    end
 
-    res.r(c,:) = fit(model);
+    res.r(c,:) = feval(fitmethod,model,fitmethodargs{:});
 
     if nperm > 1
-        nulldist.r(c,:,:) = permutesamples(model,nperm,'fit');
+        nulldist.r(c,:,:) = permutesamples(model,nperm,fitmethod,[],...
+            fitmethodargs{:});
     end
 
     if nboot > 0
-        bootdist.r(c,:,:) = bootstrapsamples(model,nboot,'fit');
+        bootdist.r(c,:,:) = bootstrapsamples(model,nboot,fitmethod,[],...
+            fitmethodargs{:});
+    end
+
+    if ~isempty(customfun)
+        res.custom{c} = feval(customfun,model);
     end
 
 end % c npredictor
