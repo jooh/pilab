9581716ef6206d64ff17811b3b68675acd73e988
Johan Carlin <johan.carlin@gmail.com>
2014-08-21 14:47:32 +0100

    return in struct form if that was the input format

--- rsa/rdm2fir.m
+++ rsa/rdm2fir.m
@@ -1,19 +1,20 @@
-% firdm = rdm2fir(rdm)
-function firdm = rdm2fir(rdm)
+% [firdm,outnames] = rdm2fir(rdm)
+function [firdm,outnames] = rdm2fir(rdm)
+
+name = 'firdm';
+wasstruct = false;
+if isstruct(rdm)
+    name = rdm(1).name;
+    wasstruct = true;
+end
 
 rdvec = asrdmvec(rdm);
 [ndis,nrdm] = size(rdvec);
 assert(nrdm==1,'only one input RDM is supported');
-
-if isstruct(rdm)
-    name = rdm.name;
-else
-    name = 'firdm';
-end
+assert(all(std(rdvec)>0),'invariant rdms detected');
 
 u = unique(rdvec);
 u(isnan(u)) = [];
-u(u==0) = [];
 nfir = numel(u);
 
 for f = 1:nfir
@@ -21,4 +22,9 @@ for f = 1:nfir
     newvec(rdvec==u(f)) = 1;
     firdm(f) = struct('name',sprintf('%s_f%02.0f',name,u(f)),...
         'RDM',vec2rdm(newvec));
+end
+
+outnames = {firdm.name};
+if ~wasstruct
+    firdm = asrdmmat(firdm);
 end
