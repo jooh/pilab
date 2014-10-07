6a2abb6ce83ae7c76fba59c5ac40e68c539b10ae
Johan Carlin <johan.carlin@gmail.com>
2014-10-07 14:24:11 +0100

    MeanRSA now support multiple (non-overlapping) predictors

--- MeanRSA.m
+++ MeanRSA.m
@@ -1,21 +1,28 @@
-% RSA sub-class for estimating mean dissimilarity. The modelrdm input is
-% used as a NaN mask to optionall estimate the mean over a subset of
-% dissimilarities. Assumes that you're using ldtRDMs or something
-% similarly symmetrical about 0 under the null.
+% RSA sub-class for estimating mean dissimilarity. We estimate the mean
+% over non-zero entries in each model RDM (note that the magnitude of the
+% original dissimilarity is discarded).
+%
+% Note that we crash if there are overlapping valid dissimilarities across
+% models, since the resulting estimates are not means in this case. If you
+% want to estimate such overlapping means you will need to use separate
+% models.
 %
 % gl = MeanRSA(modelrdms,datardms)
 classdef MeanRSA < RSA
     methods
         function gl = MeanRSA(modelrdms,datardms)
             gl = gl@RSA(modelrdms,datardms);
-            % check that X is of the correct type for this class
-            assert(gl.npredictors == 1,...
-                'only one model RDM is supported');
-            uv = unique(gl.X);
-            assert(length(uv)==1,...
-                'model RDM must contain exactly 1 unique dissimilarity');
-            % means are easy
-            gl.X(:) = 1;
+            % so we need to do some pre-processing to make this work
+            for reg = 1:gl.npredictors
+                uv = unique(gl.X(gl.X(:,reg)~=0,reg));
+                assert(numel(uv)==1,['each model RDM must contain ' ...
+                    'exactly 1 unique non-zero dissimilarity']);
+                blips = gl.X(:,reg) == uv;
+                gl.X(blips,reg) = 1;
+                gl.X(~blips,reg) = 0;
+            end
+            assert(~any(sum(gl.X,2)>1),...
+                'overlapping dissimilarities across model RDMs detected');
         end
     end
 end
