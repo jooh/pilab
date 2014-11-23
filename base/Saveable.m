fc7301efdfc1316b9f0fb4a4cab79669fda1aa80
Johan Carlin <johan.carlin@gmail.com>
2014-11-23 10:36:09 +0000

    documentation

--- base/Saveable.m
+++ base/Saveable.m
@@ -1,3 +1,10 @@
+% Base class for supporting struct conversion as part of save and load with
+% obj2struct and struct2obj. This tends to speed up saving of handle class
+% instances. Note that this trick only works with object classes that have
+% only public, writeable fields and support initialisation with no input
+% arguments.
+%
+% Saveable < matlab.mixin.Copyable
 classdef Saveable < matlab.mixin.Copyable
     methods
         function outstruct = saveobj(self)
