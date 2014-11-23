41bd032e9c235294cf244f99fa47163bb7011865
Johan Carlin <johan.carlin@gmail.com>
2014-11-23 10:34:43 +0000

    support for model RDMs with NaNs, correct calculation of partial rho

--- PartialRankRSA.m
+++ PartialRankRSA.m
@@ -1,19 +1,19 @@
 % Partial Spearman rho RSA.
 % gl = PartialRankRSA(modelrdms,datardms,partialrdms)
-classdef PartialRankRSA < RSA
+classdef PartialRankRSA < PearsonRSA
     methods
         function gl = PartialRankRSA(modelrdms,datardms,partialrdms)
             % partial r is basically the r between modelrdms and datardms
             % after partialrdms has been fitted and subtracted from each.
             tempmodel = RankRSA(partialrdms,modelrdms);
-            resmodel = residuals(tempmodel);
+            resmodel = residualsfull(tempmodel);
             tempdata = RankRSA(partialrdms,datardms);
-            resdata = residuals(tempdata);
+            resdata = residualsfull(tempdata);
             % then use super-class to initialise            
             % NB, confusingly, we DON'T want to use RankRSA as superclass
             % here because then we would incur another rank transform of
             % the residuals. 
-            gl = gl@RSA(resmodel,resdata);
+            gl = gl@PearsonRSA(resmodel,resdata);
         end
     end
 end
