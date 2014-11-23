bd6b72d78166dab80eb7c494e5acb35fda925bdc
Johan Carlin <johan.carlin@gmail.com>
2014-11-23 10:43:30 +0000

    support for new container classes

--- glm/disvol2glm.m
+++ glm/disvol2glm.m
@@ -1,5 +1,5 @@
 % Create a GLM-derived instance (by default RidgeRSA) from a set of model
-% RDMs (any pilab RSA format or a BaseVolume instance) and a cell array of
+% RDMs (any pilab RSA format or a Volume instance) and a cell array of
 % disvols. See also vol2glm. We assume that you want the same dissimilarity
 % design matrix for all splits.
 %
@@ -10,7 +10,7 @@ if ieNotDefined('glmclass')
     glmclass = 'RankRSA';
 end
 
-if isa(model,'BaseVolume')
+if isa(model,'Volume')
     model = model.data;
 end
 % otherwise assume you entered an appropriately structured set pilab RDMs.
