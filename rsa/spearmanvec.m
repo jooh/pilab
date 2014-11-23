11f33f8100aa626a96b529fb64b4666dc57380bd
Johan Carlin <johan.carlin@gmail.com>
2014-11-23 10:56:48 +0000

    use faster ranktrans function

--- rsa/spearmanvec.m
+++ rsa/spearmanvec.m
@@ -2,7 +2,7 @@
 % where one input (a) is to be correlated with a set of inputs (b), e.g. a
 % predictor RDM correlated against each of a set of searchlight RDMs.
 %
-% Uses pearsonvec on tiedrank transformed data. The key slow-down in this
+% Uses pearsonvec on ranktrans transformed data. The key slow-down in this
 % function relative to pearsonvec is the rank transform, which iterates. So
 % in some applications (e.g., fitting the same ranks to different
 % predictors), further gains can be had by first rank transforming and
@@ -10,4 +10,4 @@
 % r = spearmanvec(a,b)
 function r = spearmanvec(a,b)
 
-r = pearsonvec(tiedrank(a),tiedrank(b));
+r = pearsonvec(ranktrans(a),ranktrans(b));
