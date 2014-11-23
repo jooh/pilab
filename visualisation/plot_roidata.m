fc3c15db393d557484f79f7d2661ba2d5491a53f
Johan Carlin <johan.carlin@gmail.com>
2014-11-23 11:03:43 +0000

    now uses glasbey colormap for more distinctive bar colors, attempts to break up ROIs and conditions by prefixes

--- visualisation/plot_roidata.m
+++ visualisation/plot_roidata.m
@@ -1,5 +1,7 @@
 % standard bar chart visualisations for results from some roidata struct
-% (output from e.g. roidata_rsa).
+% (output from e.g. roidata_rsa). Detects particular prefixes ('l_', 'r_'
+% for ROIs, 'contrast_' for conditions) and separates these into separate
+% figures in sub-directories.
 %
 % plot_roidata(figdir,res,groupres,varargin)
 function plot_roidata(figdir,res,groupres,varargin)
@@ -9,14 +11,20 @@ function plot_roidata(figdir,res,groupres,varargin)
 mkdirifneeded(figdir);
 
 [ncon,nroi] = size(m);
-nsub = numel(res.z_subject);
+if isfield(res,'z_subject')
+    nsub = numel(res.z_subject);
+else
+    nsub = [];
+end
 
-roicolors = hsv(nroi) * .9;
-concolors = hsv(ncon) * .9;
+% non-overlapping ROI and condition colours from glasbey colour map
+roicolors = cmap_glasbey(nroi);
+concolors = cmap_glasbey(ncon+nroi);
+concolors(1:nroi,:) = [];
 
 % invisible figure
 fighand = figurebetter([],[30 30],1/2,false);
-% make it current fig
+% make it current fig (breaks invisibility though)
 figure(fighand);
 % overlay singles in glasbey map
 set(gcf,'defaultaxescolororder',cmap_glasbey(nsub));
@@ -24,74 +32,131 @@ set(gcf,'defaultaxescolororder',cmap_glasbey(nsub));
 conclean = stripbadcharacters(res.rows_contrast,' ');
 roiclean = stripbadcharacters(res.cols_roi);
 
-% one chart per ROI
-for r = 1:nroi
-    % awkwardly positioned plot to insure tons of white space so rotated x
-    % labels don't get cropped out
-    subplot(4,4,[6:7 10:11]);
-    thisroi = roiclean{r};
-    thism = m(:,r);
-    thiserr = errs(:,r);
-    thisp = p(:,r);
-    [fh,bh,eh,th] = plotsub(fighand,thism,thiserr,thisp,concolors,conclean);
-    ylabel(ylab);
-    title(thisroi);
-    box off
-    printstandard(fullfile(figdir,sprintf('byroi_%s',thisroi)));
-    % singles analysis
-    if ~isempty(groupres)
-        hold on;
-        ph = plot(1:length(thism),squeeze(groupres.r(:,r,:)),'.',...
-            'markersize',12);
-        % set barchart underneath to outlines for clarity
-        set(bh,'facecolor',[1 1 1],'edgecolor',[0 0 0]);
-        % add a legend
-        L = legend(ph,res.z_subject,'plotboxaspectratiomode','auto');
-        lax = subplot(4,4,12);
-        centerinaxis(L,lax);
-        set(L,'box','off');
-        delete(eh);
-        delete(th);
-        printstandard(fullfile(figdir,sprintf('singles_byroi_%s',thisroi)));
-    end
-    clf(fighand);
+contrastind = find(cellfun(@(thisc)strcmp('contrast_',...
+    thisc(1:min([9 numel(thisc)]))),res.rows_contrast));
+allconind = 1:ncon;
+notcontrastind = setdiff(allconind,contrastind);
+
+congroups = struct('name','bycon_','ind',notcontrastind,'prefix','');
+if any(contrastind)
+    congroups(end+1) = struct('name','bycon_contrast_','ind',...
+        contrastind,'prefix','\Delta');
 end
 
-% one chart per analysis
-for c = 1:ncon
-    subplot(4,4,[6:7 10:11]);
-    thiscon = stripbadcharacters(conclean{c},'_');
-    thism = m(c,:);
-    thiserr = errs(c,:);
-    thisp = p(c,:);
-    [fh,bh,eh,th] = plotsub(fighand,thism,thiserr,thisp,roicolors,roiclean);
-    ylabel(ylab);
-    title(conclean{c});
-    box off
-    printstandard(fullfile(figdir,sprintf('bycon_%s',thiscon)));
-    % singles analysis
-    if ~isempty(groupres)
-        hold on;
-        ph = plot(1:length(thism),squeeze(groupres.r(c,:,:)),'.',...
-            'markersize',12);
-        % set barchart underneath to outlines for clarity
-        set(bh,'facecolor',[1 1 1],'edgecolor',[0 0 0]);
-        % add a legend
-        L = legend(ph,res.z_subject,'plotboxaspectratiomode','auto');
-        lax = subplot(4,4,12);
-        centerinaxis(L,lax);
-        set(L,'box','off');
-        delete(eh);
-        delete(th);
-        printstandard(fullfile(figdir,sprintf('singles_bycon_%s',thiscon)));
-    end
-    clf(fighand);
+leftind = find(cellfun(@(thisc)strcmp('l_',thisc(1:min([2 numel(...
+    thisc)]))),res.cols_roi));
+rightind = find(cellfun(@(thisc)strcmp('r_',thisc(1:min([2 numel(...
+    thisc)]))),res.cols_roi));
+notleftright = setdiff(1:nroi,[leftind rightind]);
+
+roigroups = struct('name','byroi_bi_','ind',notleftright);
+if any(leftind)
+    roigroups(end+1) = struct('name','byroi_left_','ind',leftind);
 end
 
+if any(rightind)
+    % nb we don't change the prefix because l_ and r_ are already in file
+    % name
+    roigroups(end+1) = struct('name','byroi_right_','ind',rightind);
+end
+
+logstr('generating figures for %d condition groups and %d roi groups\n',...
+    numel(congroups),numel(roigroups));
+
+for rgroup = 1:numel(roigroups)
+    thisr = roigroups(rgroup);
+    thisroicolor = roicolors(1:numel(thisr.ind),:);
+    for cgroup = 1:numel(congroups)
+        thisc = congroups(cgroup);
+        thisylab = ylab;
+        if ~isempty(thisc.prefix)
+            if iscell(thisylab)
+                thisylab{1} = [thisc.prefix thisylab{1}];
+            else
+                thisylab = [thisc.prefix thisylab];
+            end
+        end
+        thisconcolor = concolors(1:numel(thisc.ind),:);
+        % set the figdir
+        thisfigdir = fullfile(figdir,[thisr.name thisc.name]);
+        mkdirifneeded(thisfigdir);
+
+        % plot each group separately
+        for r = thisr.ind
+            % awkwardly positioned plot to insure tons of white space so rotated x
+            % labels don't get cropped out
+            subplot(4,4,[6:7 10:11]);
+            thisroi = roiclean{r};
+            thism = m(thisc.ind,r);
+            thiserr = errs(thisc.ind,r);
+            thisp = p(thisc.ind,r);
+            [fh,bh,eh,th] = plotsub(fighand,thism,thiserr,thisp,...
+                thisconcolor,conclean(thisc.ind));
+            ylabel(thisylab);
+            title(thisroi);
+            box off
+            printstandard(fullfile(thisfigdir,sprintf('%s_%s',...
+                thisr.name,thisroi)));
+            % singles analysis
+            if ~isempty(groupres)
+                hold on;
+                ph = plot(1:length(thism),squeeze(groupres.r(thisc.ind,r,:)),'.',...
+                    'markersize',12);
+                % set barchart underneath to outlines for clarity
+                set(bh,'facecolor',[1 1 1],'edgecolor',[0 0 0]);
+                % add a legend
+                L = legend(ph,res.z_subject,'plotboxaspectratiomode','auto');
+                lax = subplot(4,4,12);
+                centerinaxis(L,lax);
+                set(L,'box','off');
+                delete(eh);
+                delete(th);
+                printstandard(fullfile(thisfigdir,sprintf(...
+                    'singles_%s_%s',thisr.name,thisroi)));
+            end
+            clf(fighand);
+        end % for r = thisr.ind(:)'
+
+        % one chart per analysis
+        for c = 1:thisc.ind
+            subplot(4,4,[6:7 10:11]);
+            thiscon = stripbadcharacters(conclean{c},'_');
+            thism = m(c,thisr.ind);
+            thiserr = errs(c,thisr.ind);
+            thisp = p(c,thisr.ind);
+            [fh,bh,eh,th] = plotsub(fighand,thism,thiserr,thisp,...
+                thisroicolor,roiclean(thisr.ind));
+            ylabel(thisylab);
+            title(conclean{c});
+            box off
+            printstandard(fullfile(thisfigdir,sprintf('bycon_%s_%s',...
+                thiscon,thisr.name)));
+            % singles analysis
+            if ~isempty(groupres)
+                hold on;
+                ph = plot(1:length(thism),squeeze(...
+                    groupres.r(c,thisr.ind,:)),'.',...
+                    'markersize',12);
+                % set barchart underneath to outlines for clarity
+                set(bh,'facecolor',[1 1 1],'edgecolor',[0 0 0]);
+                % add a legend
+                L = legend(ph,res.z_subject,...
+                    'plotboxaspectratiomode','auto');
+                lax = subplot(4,4,12);
+                centerinaxis(L,lax);
+                set(L,'box','off');
+                delete(eh);
+                delete(th);
+                printstandard(fullfile(thisfigdir,...
+                    sprintf('singles_bycon_%s_%s',thiscon,thisr.name)));
+            end
+            clf(fighand);
+        end % for c = 1:thisc.ind(:)
+    end % for cgroup = 1:numel(congroups)
+end % for rgroup = 1:numel(roigroups)
 close(fighand);
 
 function [fh,bh,eh,th] = plotsub(fighand,thism,thiserr,thisp,concolors,conclean)
-
 [fh,bh,eh] = barchart(thism,'errors',thiserr,'facecolor',concolors,...
     'x',1:length(thism),'labels',conclean,'fighand',fighand);
 % nansum here because errs can be all NaN
