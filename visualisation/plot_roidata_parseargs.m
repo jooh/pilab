276c7d17db0dab701949cb994a21b3e74a5e71bb
Johan Carlin <johan.carlin@gmail.com>
2015-02-05 17:31:06 +0000

    support new handling of groupres

--- visualisation/plot_roidata_parseargs.m
+++ visualisation/plot_roidata_parseargs.m
@@ -2,12 +2,25 @@
 % plotting. This function is mainly useful if you want to use a custom
 % plugin plot function in e.g. aamod_pilab_rsa_visualisation.
 %
-% [m,errs,p,ylab] = plot_roidata_parseargs(res,varargin)
-function [m,errs,p,ylab] = plot_roidata_parseargs(res,varargin)
+% [m,errs,p,ylab,groupm,groupp] = plot_roidata_parseargs(res,groupres,varargin)
+function [m,errs,p,ylab,groupm,groupp] = plot_roidata_parseargs(res,groupres,varargin)
+
 
 mlabel = [];
 getArgs(varargin,{'mtarget','mean','errtarget',[],'ptarget','ppara',...
-    'mlabel','','errlabel',''},'suppressUnknownArgMessage=1');
+    'mlabel','','errlabel','','groupmtarget','r','groupptarget',...
+    'pperm'},'suppressUnknownArgMessage=1');
+
+if ieNotDefined('groupres')
+    groupm = [];
+    groupp = [];
+else
+    % this extra test is mainly to make it easy to spot code where the call
+    % syntax needs to be updated
+    assert(isstruct(groupres),'groupres must be second input');
+    groupm = groupres.(groupmtarget);
+    groupp = groupres.(groupptarget);
+end
 
 % fill in a dummy label if nothing else is specified
 if isempty(mlabel)
