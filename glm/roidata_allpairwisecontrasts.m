6d361d49cf31258ed2f7b75b9add644d18d76dde
Johan Carlin <johan.carlin@gmail.com>
2015-08-25 12:08:30 +0100

    support convec field in place of conplus/conminus

--- glm/roidata_allpairwisecontrasts.m
+++ glm/roidata_allpairwisecontrasts.m
@@ -1,8 +1,12 @@
 % return a contrasts struct for roidata_rfx containing all pairwise
 % contrasts between the conditions in the cell array connames.
 %
-% contrasts = roidata_allpairwisecontrasts(connames)
-function contrasts = roidata_allpairwisecontrasts(connames)
+% contrasts = roidata_allpairwisecontrasts(connames,convecmode)
+function contrasts = roidata_allpairwisecontrasts(connames,convecmode)
+
+if ieNotDefined('convecmode')
+    convecmode = false;
+end
 
 ncon = numel(connames);
 conmat = allpairwisecontrasts(ncon);
@@ -14,7 +18,11 @@ for c = 1:ncontrast
     conn = connames(thiscon==-1);
     assert(numel(conp)==1 && numel(conn)==1,'contrast indexing failed');
     contrasts(c).name = sprintf('contrast_%s_o_%s',conp{1},conn{1});
-    contrasts(c).conplus = conp;
-    contrasts(c).conminus = conn;
+    if convecmode
+        contrasts(c).convec = conmat(c,:);
+    else
+        contrasts(c).conplus = conp;
+        contrasts(c).conminus = conn;
+    end
     contrasts(c).tail = 'both';
 end
