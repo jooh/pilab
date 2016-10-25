a1f06e8b6c8e8b005ecc6176d0c8bce1576555fa
Johan Carlin <johan.carlin@gmail.com>
2016-10-25 21:50:16 +0100

    documentation, inherit support for multiple RDMs from table2csv stack functionality

--- rsa/rdm2csv.m
+++ rsa/rdm2csv.m
@@ -1,21 +1,22 @@
 % Write out the RDM as a CSV file. Any varargin are passed to table2csv.
 %
+% INPUT     DEFAULT     DESCRIPTION
+% rdm       -           something where isrdm returns true
+% outpath   -           write CSV to this path
+% names     {}          used as both row and column labels
+% varargin  {}          any additional inputs are passed to table2csv.
+%                           zlabels is especially useful for writing out
+%                           multiple RDMs into the same file.
+%
 % rdm2csv(rdm,outpath,[names],[varargin])
 function rdm2csv(rdm,outpath,names,varargin)
 
 if ieNotDefined('names')
-    names = [];
-end
-
-if isstruct(rdm) & isempty(names)
-    names = {rdm.name};
+    names = {};
 end
 
 % make sure mat from here on
 rdm = asrdmmat(rdm);
-
-[nc,~,nrdm] = size(rdm);
-assert(nrdm==1,'only 1 input RDM can be entered per call');
 
 % NB need to insert extra column label to account for rowlabels
 table2csv(rdm,outpath,'rowlabels',ascol(names),...
