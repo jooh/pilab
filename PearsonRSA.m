f264cbd30bfefef184bcf6aae851f7a5c0fdf996
Johan Carlin <johan.carlin@gmail.com>
2017-04-04 15:59:50 +0100

    documentation

--- PearsonRSA.m
+++ PearsonRSA.m
@@ -1,12 +1,14 @@
-% GLM sub-class for representational similarity analysis.
+% Pearson r-based representational similarity analysis. See also the RSA, GLM
+% superclasses.
+%
 % gl = PearsonRSA(modelrdms,datardms)
 classdef PearsonRSA < RSA
     methods
         function gl = PearsonRSA(modelrdms,datardms)
             % use super-class to initialise            
             gl = gl@RSA(modelrdms,datardms);
-            % then rank trans and Z score so that linear fits become
-            % equivalent to Spearman rho
+            % then Z score so that linear fits become
+            % equivalent to Pearson r
             gl.X = zscore(gl.X,0,1);
             gl.data = zscore(gl.data,0,1);
         end
